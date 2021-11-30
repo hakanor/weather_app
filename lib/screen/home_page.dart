803ee4d4eb7628ab35cee79c99f1eb953e1e324d
Your Name <hakanor99@gmail.com>
2021-11-30 20:23:20 +0300

    Home page created

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:weather_app/widgets/weather_card_widget.dart';
+
 
 
 class home_page extends StatefulWidget {
@@ -15,14 +16,37 @@ class _home_pageState extends State<home_page> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: Text("Hava Durumu anasayfa", style: TextStyle(color: Colors.black),),
-        shadowColor: Colors.transparent,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){
+          // buraya fab tıklama eventleri gelecek
+        },
       ),
-      body: Container(
-        color: Colors.grey,
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(horizontal: 24,vertical: 20),
+          color: Color(0xFFF6F6F6),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: EdgeInsets.only(top:20,),
+                child: Image(image: AssetImage('assets/images/logo.png'),width: 70,height: 70,),
+              ),
+              Expanded(
+                child: ListView(
+                  children: [
+                    WeatherCardWidget(),
+                    WeatherCardWidget(),
+                    WeatherCardWidget(),
+                  ],
+                ),
+              )
+            ],
+
+            ),
+          ),
       ),
     );
   }
